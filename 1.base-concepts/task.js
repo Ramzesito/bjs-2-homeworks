83c541c4b8947d50b4aba8db8fb053cefa87c6c6
Ramzesito <roman_shv@mail.ru>
2023-09-24 15:05:52 +0300

    first task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discr = b**2 - 4 * a * c;
+
+  if (discr > 0) {
+    arr.push((-b + Math.sqrt(discr) )/(2*a));
+    arr.push((-b - Math.sqrt(discr) )/(2*a));
+  }
+  else if (discr === 0) {
+    arr.push(-b/(2*a));
+  }
+  else /* discr < 0 */ {
+    // ничего не делать
+  }
+    
   return arr;
 }
 
