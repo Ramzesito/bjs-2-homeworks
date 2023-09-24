5c88fbe25fda57da0723726e0dc58a4eb039c679
Ramzesito <roman_shv@mail.ru>
2023-09-24 15:22:18 +0300

    second task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,26 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  let discr = b**2 - 4 * a * c;
 
-  if (discr > 0) {
-    arr.push((-b + Math.sqrt(discr) )/(2*a));
-    arr.push((-b - Math.sqrt(discr) )/(2*a));
-  }
-  else if (discr === 0) {
-    arr.push(-b/(2*a));
-  }
-  else /* discr < 0 */ {
-    // ничего не делать
-  }
-    
-  return arr;
+function solveEquation(a, b, c) {
+	let arr = [];
+	let discr = b ** 2 - 4 * a * c;
+
+	if (discr > 0) {
+		arr.push((-b + Math.sqrt(discr)) / (2 * a));
+		arr.push((-b - Math.sqrt(discr)) / (2 * a));
+	} else if (discr === 0) {
+		arr.push(-b / (2 * a));
+	} else /* discr < 0 */ {
+		// ничего не делать
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let s = amount - contribution;
+	let p = (percent * 0.01) / 12;
+	let payment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let sum = payment * countMonths;
+
+	return +sum.toFixed(2);
 }
