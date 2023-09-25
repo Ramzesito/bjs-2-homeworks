fd493d20f9cf0a98c9abbc9b455048e6b1b4c825
Ramzesito <roman_shv@mail.ru>
2023-09-25 15:53:57 +0300

    first task completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,20 @@
 function getArrayParams(...arr) {
 
+  if (arr.length === 0) { return null; }
+  let min = arr[0];
+  let max = arr[0];
+  let sum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+        max = arr[i];
+    }
+    sum += arr[i];
+  }
+  let avg = +(sum / arr.length).toFixed(2);
+
   return { min: min, max: max, avg: avg };
 }
 
