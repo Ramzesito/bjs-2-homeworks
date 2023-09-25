2f8641605388859e8b3368daec4234df26825f0e
Ramzesito <roman_shv@mail.ru>
2023-09-25 17:49:43 +0300

    second task completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -19,19 +19,36 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  return arr.reduce(function (currentSum, currentElement) {
+    return currentSum + currentElement;
+  }, 0)
 
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  return ((arr.length === 0) ? 0 : Math.max(...arr) - Math.min(...arr));
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    (arr[i] % 2 === 0) ? (sumEvenElement += arr[i]) : (sumOddElement += arr[i]);
+  }
+  return (sumEvenElement - sumOddElement);
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) { return 0; }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  return (sumEvenElement / countEvenElement);
 }
 
 function makeWork (arrOfArr, func) {
