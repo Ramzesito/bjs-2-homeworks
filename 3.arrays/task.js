6839e59643577ac0cf7c87cc3bb8d1cce15d8456
Ramzesito <roman_shv@mail.ru>
2023-09-27 17:06:09 +0300

    first task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,12 @@
 function compareArrays(arr1, arr2) {
   
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((idx) => {
+        return (arr1[idx] === arr2[idx]);  
+    });
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
