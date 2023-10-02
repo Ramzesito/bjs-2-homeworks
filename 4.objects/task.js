2dba4f35d4c10e3355b57031c00c16650a1bece0
Ramzesito <roman_shv@mail.ru>
2023-10-02 16:25:59 +0300

    task completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = new Array(0);
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subjectName = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")) {
+    this.marks = marks;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ((this.hasOwnProperty("marks")) && (this.marks !== [])) {
+        return this.marks.reduce((acc, mark, idx, arr) => acc + mark / arr.length, 0);
+    }
+    return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subjectName;
+  delete this.marks;
+  this.reason = reason;
 }
