2a442968fd7640cf736141d5f4d2cb7589c3c4eb
Ramzesito <roman_shv@mail.ru>
2023-10-21 21:43:23 +0300

    1-2 task completed (part 8 - decorators)

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    return (...args) => {
+        const hash = md5(args);
+        let found = cache.find(item => item.hash === hash);
+        if (found) {
+            let result = found['result'];
+            console.log(`Из кеша: ${result}`);
+            return `Из кеша: ${result}`;
+        }
+        if (cache.length >= 5) {
+            cache.shift();
+        }
+        const result = func(...args);
+        console.log(`Вычисляем: ${result}`);
+        cache.push({hash, result});
+        return `Вычисляем: ${result}`;
+    }
 }
+
+//const addAndMultiply = (a, b, c) => (a + b) * c;
+//const upgraded = cachingDecoratorNew(addAndMultiply);
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if (!timeoutId) {
+            func(...args);
+            wrapper.count++;
+        }
+        else {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func(...args);
+            wrapper.count++;
+        }, delay);
+    }
+    return wrapper;
 }
